6f051d5482101c14943260852d18efc5f2f4f6e6
dzejkop <jakubtrad@gmail.com>
2026-09-23 11:43:26 +0200

    Make verifier client unit tests portable to WASM

--- nix/dev-shells.nix
+++ nix/dev-shells.nix
@@ -21,21 +21,56 @@ lib.genAttrs
         inherit system;
         overlays = [ rust-overlay.overlays.default ];
       };
+      rustToolchain = (pkgs.rust-bin.fromRustupToolchainFile (root + "/rust-toolchain.toml")).override {
+        targets = [ "wasm32-unknown-unknown" ];
+      };
+      llvm = pkgs.llvmPackages;
+      wasmBindgenCli = pkgs.rustPlatform.buildRustPackage rec {
+        pname = "wasm-bindgen-cli";
+        version = "0.2.126";
+        src = pkgs.fetchurl {
+          name = "${pname}-${version}.tar.gz";
+          url = "https://static.crates.io/crates/${pname}/${pname}-${version}.crate";
+          hash = "sha256-ji6/bu+Hw05mI0fx3d++pUEwS7cpRxHtLCrNh0bMW1A=";
+        };
+        cargoHash = "sha256-VucqkXbCi4qtQzY/HrXiDnbSURsagPsdNVMn1Tw3UiY=";
+        doCheck = false;
+      };
+      firefoxAvailable = lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.firefox;
+      firefoxBinary = if pkgs.stdenv.isDarwin then
+        "${pkgs.firefox}/Applications/Firefox.app/Contents/MacOS/firefox"
+      else
+        "${pkgs.firefox}/bin/firefox";
     in
     {
-      default = pkgs.mkShell {
+      default = pkgs.mkShell ({
         buildInputs = lib.optionals pkgs.stdenv.isLinux [
           pkgs.minijail
           pkgs.libcap
         ];
         packages = with pkgs; [
-          (rust-bin.fromRustupToolchainFile (root + "/rust-toolchain.toml"))
+          rustToolchain
           clang
           protobuf
           pkg-config
           jq
+          llvm.clang-unwrapped
+          llvm.bintools-unwrapped
+          wasmBindgenCli
+          binaryen
+          nodejs
+        ] ++ lib.optionals firefoxAvailable [
+          firefox
+          geckodriver
         ];
-        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-      };
+        LIBCLANG_PATH = "${llvm.libclang.lib}/lib";
+        CC_wasm32_unknown_unknown = "${llvm.clang-unwrapped}/bin/clang";
+        AR_wasm32_unknown_unknown = "${llvm.bintools-unwrapped}/bin/llvm-ar";
+        CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUNNER = "${wasmBindgenCli}/bin/wasm-bindgen-test-runner";
+        WASM_BINDGEN_USE_BROWSER = "1";
+      } // lib.optionalAttrs firefoxAvailable {
+        GECKODRIVER = "${pkgs.geckodriver}/bin/geckodriver";
+        GECKODRIVER_ARGS = "--binary ${firefoxBinary}";
+      });
     }
   )
